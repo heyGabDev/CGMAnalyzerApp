2c144ff3dba4c5ec95b1460d77453a23a6fcfdf1
heyGabDev <heygab.consulting@gmail.com>
2025-07-07 15:07:36 +0200

    <wip>(<CGM FILES>): <Replace front project : Delete MAUI and create WPF + Connection API>

--- CGMAnalyzer.API/Controllers/CGMController.cs
+++ CGMAnalyzer.API/Controllers/CGMController.cs
@@ -1,4 +1,5 @@
 ﻿using CGMAnalyzer.API.Modeles;
+using CGMAnalyzer.API.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CGMAnalyzer.API.Controllers;
@@ -8,24 +9,35 @@ namespace CGMAnalyzer.API.Controllers;
 public class CGMController : ControllerBase
 {
     [HttpPost("import")]
-    public async Task<IActionResult> Import(List<IFormFile> files)
+    public async Task<IActionResult> ImportCgmFile(List<IFormFile> files, [FromServices]ICgmConverter cgmConverter)
     {
-        if (files == null || files.Count == 0)
-            return BadRequest("Aucun fichier reçu.");
-
-        var results = new List<CGMResult>();
-
-        foreach (var file in files)
+        try
         {
-            // Simuler un traitement
-            var result = new CGMResult
+            if (files == null || files.Count == 0)
+            return BadRequest($"Aucun fichier reçu.");
+
+            var results = new List<CGMResult>();
+
+            foreach (var file in files)
             {
-                FileName = file.FileName,
-                BmpPath = "/images/" + file.FileName + ".bmp",
-                Errors = new List<string> { "Erreur fictive pour test" }
-            };
-            results.Add(result);
+                var bmpPath = await cgmConverter.ConvertToBmpAsync(file);
+                var result = new CGMResult
+                {
+                    FileName = file.FileName,
+                    BmpPath = bmpPath,
+                    Errors = new List<string> { $"Pas d'erreur" }
+                };
+                results.Add(result);
+            }
+            return Ok(results.First()); // Pour test (un fichier à la fois)
         }
-        return Ok(results.First()); // Pour test MAUI (un fichier à la fois)
+        catch (Exception err)
+        {
+            //Logg err
+            Console.WriteLine($"Import error : {err.Message}");
+            return StatusCode(500, $"Servor error : {err.Message}");
+        }
+
+        
     }
 }
